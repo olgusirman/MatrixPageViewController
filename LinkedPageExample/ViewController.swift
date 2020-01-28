5f13f4bc60d3ad069a8c8e84eb64415844f58357
Olgu <olgusirman@gmail.com>
2020-01-28 17:36:07 +0000

    Initial Commit

---
+++ LinkedPageExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkedPageExample
+//
+//  Created by Olgu on 28.01.2020.
+//  Copyright © 2020 Aspendos IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
