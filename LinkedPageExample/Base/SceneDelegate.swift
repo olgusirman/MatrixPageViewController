f799a1a2494af1be98c431dba8fabfe93b4a2e77
Olgu <olgusirman@gmail.com>
2020-02-05 22:13:50 +0000

    unnecessary SceneDelegate helpers removed

---
+++ LinkedPageExample/Base/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  LinkedPageExample
+//
+//  Created by Olgu on 28.01.2020.
+//  Copyright © 2020 Aspendos IT. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
